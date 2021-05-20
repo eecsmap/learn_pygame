ba5750640b9e545fef816b53db16f551af9838d1
Wenyang Duan <wenyang.duan@gmail.com>
2021-05-20 00:07:38 -0700

    improve the calculation of surface center.

--- demo/demo_mouse.py
+++ demo/demo_mouse.py
@@ -5,7 +5,7 @@ screen = pygame.display.set_mode((400, 400))
 
 mouse_raw = pygame.image.load('star.png').convert_alpha()
 mouse_surface = pygame.transform.scale(mouse_raw, (20, 20))
-
+mouse_surface_rect = mouse_surface.get_rect()
 run = True
 pygame.mouse.set_visible(False)
 while run:
@@ -14,7 +14,7 @@ while run:
             run = False
     x, y = pygame.mouse.get_pos()
     screen.fill('black')
-    screen.blit(mouse_surface, (x - mouse_surface.get_width() / 2, y - mouse_surface.get_height() / 2))
+    screen.blit(mouse_surface, (x - mouse_surface_rect.centerx, y - mouse_surface_rect.centery))
     pygame.display.flip()
 
 pygame.quit()
